329e244aeccf2678d0789cf8275e6e6dc0f0dc68
alexhwoods <alex@makeswift.com>
2024-06-17 09:46:23 -0400

    chore: change font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Button from "./Button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col justify-center items-center">
+    <main className="flex min-h-screen flex-col justify-center items-center font-mono">
       <Button />
     </main>
   );
