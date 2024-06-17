1aa8406d04beaca93f89728ecb538a35f5289e13
alexhwoods <alex@makeswift.com>
2024-06-17 16:23:39 -0400

    fix: use form for progressive enhancement

--- src/app/Button.tsx
+++ src/app/Button.tsx
@@ -1,18 +1,18 @@
-'use client'
- 
-import { goToOtherPage } from './actions'
- 
+"use client";
+
+import { goToOtherPage } from "./actions";
+
 export default function Button() {
   return (
     <>
-      <button
-      className="border-2 rounded-sm border-white py-2 px-4"
-        onClick={async () => {
-          await goToOtherPage()
-        }}
-      >
-        Click me
-      </button>
+      <form>
+        <button
+          formAction={goToOtherPage}
+          className="border-2 rounded-sm border-white py-2 px-4"
+        >
+          Click me
+        </button>
+      </form>
     </>
-  )
+  );
 }
